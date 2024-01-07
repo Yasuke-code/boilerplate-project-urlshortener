10b507fac799b6c407b17e2d12552de2f2be1efd
matan solom <matan.s.dev@gmail.com>
2024-01-07 13:23:57 +0200

    First mission

--- index.js
+++ index.js
@@ -4,9 +4,11 @@ const cors = require('cors');
 const app = express();
 
 // Basic Configuration
-const port =3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(express.urlencoded({extended:true}))
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -17,6 +19,41 @@ app.get('/', function(req, res) {
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
+});
+function isValidUrl(string) {
+  try {
+    new URL(string);
+    return true;
+  } catch (err) {
+    return false;
+  }
+}
+// {
+//   "original_url": "https://www.google.com",
+//   "short_url": 1
+//   }
+const  originalUrls= []
+const shortUrls= []
+
+app.post('/api/shorturl',(req,res)=>{
+
+  const url=req.body.url;
+  const foundUrl=originalUrls.indexOf(url)
+
+  if(foundUrl<0){
+    originalUrls.push(url)
+    shortUrls.push(shortUrls.length)
+    return res.json({
+              original_url:url,
+              short_url:shortUrls[shortUrls.length-1]
+            })
+  }
+
+  return res.json({
+    original_url:url,
+    short_url:shortUrls[foundUrl]
+  })
+
 });
 
 app.listen(port, function() {
